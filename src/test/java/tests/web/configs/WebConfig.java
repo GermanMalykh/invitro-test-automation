9e7b0fbc62414d66ccfdeaed42710a8542c5991f
germanmalykh <germanmalyh@gmail.com>
2025-08-21 15:42:07 +0300

    Draft WebTests x2 (Selenide 7.10)

--- src/test/java/tests/web/configs/WebConfig.java
+++ src/test/java/tests/web/configs/WebConfig.java
@@ -1,14 +1,24 @@
 package tests.web.configs;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class WebConfig {
 
     @BeforeAll
-    static void setup(){
+    static void setup() {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
+    }
+
+    @AfterEach
+    void clearCartFromLocalStorage() {
+        executeJavaScript("localStorage.removeItem('flockapi:2752:cart');");
+        WebDriverRunner.getWebDriver().manage().deleteCookieNamed("INVITRO_CART");
     }
 }
--- src/test/java/tests/web/pages/DesktopBasePage.java
+++ src/test/java/tests/web/pages/DesktopBasePage.java
@@ -1,16 +1,18 @@
 package tests.web.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byTitle;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
 
@@ -18,8 +20,8 @@ public class DesktopBasePage {
 
     String CART_TITLE = "В корзину";
 
-    SelenideElement HEADER_MAIN_MENU_TITLE = $(".invitro_header-menu_main-item"),
-            CART_PRODUCTS_NAME = $("[class*='CartProductsPanel_cartProducts']");
+    SelenideElement HEADER_MAIN_MENU_TITLE = $(".invitro_header-menu_main-item");
+    ElementsCollection CART_ITEMS = $$("li [class*='CartProduct_product__']");
 
     public DesktopBasePage openPageByURL(String URL) {
         open(URL);
@@ -39,35 +41,10 @@ public class DesktopBasePage {
         return this;
     }
 
-    public SelenideElement selectElementByParent(String text) {
-        return $(byText(text))
-                .shouldBe(visible, Duration.ofSeconds(3))
-                .parent();
-    }
-
-    public DesktopBasePage clickByElement(SelenideElement element) {
-        executeJavaScript(
-                "const el=arguments[0];" +
-                        "const cs=getComputedStyle(el,'::before');" +
-                        "const r =el.getBoundingClientRect();" +
-                        "const right=parseFloat(cs.right)||20;" +
-                        "const bl   =parseFloat(cs.borderLeftWidth)||6;" +
-                        "const x = Math.round(r.left + r.width  - right - bl/2);" +
-                        "const y = Math.round(r.top  + r.height / 2);" +
-                        "const tgt=document.elementFromPoint(x,y) || el;" +
-                        "['pointermove','mousemove','mouseenter','mouseover'].forEach(t=>" +
-                        "  tgt.dispatchEvent(new MouseEvent(t,{clientX:x,clientY:y,bubbles:true,cancelable:true,view:window}))" +
-                        ");" +
-                        "tgt.dispatchEvent(new MouseEvent('click',{clientX:x,clientY:y,bubbles:true,cancelable:true,view:window}));",
-                element
-        );
-        return this;
-    }
-
     public DesktopBasePage selectPage(String pageNumber) {
         SelenideElement paginationElement = $(".catalog_pagination__elem--num[data-num='" + pageNumber + "']")
                 .shouldBe(visible, Duration.ofSeconds(10));
-        executeJavaScript("arguments[0].scrollIntoView({block: 'center'});", paginationElement);
+        paginationElement.scrollIntoCenter();
         paginationElement.click();
         return this;
     }
@@ -75,21 +52,25 @@ public class DesktopBasePage {
     public DesktopBasePage findProductOnPAge(String productTitle) {
         SelenideElement productCard = $$(".item_card--active").filterBy(text(productTitle)).first();
         productCard.shouldBe(visible, Duration.ofSeconds(10));
-        executeJavaScript("arguments[0].scrollIntoView({block: 'center'});", productCard);
+        productCard.scrollIntoCenter();
         return this;
     }
 
     public DesktopBasePage addProductToCart() {
         SelenideElement addToCartButton = $$(byTitle(CART_TITLE)).last();
         addToCartButton.shouldBe(visible, Duration.ofSeconds(5));
-        executeJavaScript("arguments[0].scrollIntoView({block: 'center'});", addToCartButton);
+        addToCartButton.scrollIntoCenter();
         sleep(500);
         addToCartButton.doubleClick();
         return this;
     }
 
-    public DesktopBasePage checkProductNameInCart(String productTitle) {
-        CART_PRODUCTS_NAME.shouldHave(text(productTitle));
+    public DesktopBasePage checkProductInCart(String productTitle, String expectedPrice) {
+        ElementsCollection matchingItem = CART_ITEMS
+                .filterBy(Condition.text(productTitle))
+                .shouldHave(size(1));
+        matchingItem.first().$("[class*='CartProduct_productPrice']")
+                .shouldHave(Condition.text(expectedPrice));
         return this;
     }
 }
