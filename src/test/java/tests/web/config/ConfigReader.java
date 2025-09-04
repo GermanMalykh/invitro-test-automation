538c9211c2f7994136ec10bd857b486bdaa6cf0c
germanmalykh <germanmalyh@gmail.com>
2025-09-04 13:27:54 +0300

    ConfigReader update x4

--- src/test/java/tests/web/config/ConfigReader.java
+++ src/test/java/tests/web/config/ConfigReader.java
@@ -9,48 +9,12 @@ public class ConfigReader {
 
     static {
         String env = System.getProperty("env", "local");
-        
-        // Пробуем разные пути для поиска файла
-        String[] possiblePaths = {
-            "src/test/resources/configs/web/" + env + ".properties",
-            "configs/web/" + env + ".properties",
-            System.getProperty("user.dir") + "/src/test/resources/configs/web/" + env + ".properties",
-            "/home/jenkins/workspace/C14_gnmalykh_invitro_java/src/test/resources/configs/web/" + env + ".properties"
-        };
-        
-        boolean loaded = false;
-        Exception lastException = null;
-        
-        for (String path : possiblePaths) {
-            try {
-                try (FileInputStream fis = new FileInputStream(path)) {
-                    props.load(fis);
-                    loaded = true;
-                    break;
-                }
-            } catch (IOException e) {
-                lastException = e;
-                // Попробуем через ClassLoader
-                try {
-                    var inputStream = ConfigReader.class.getClassLoader().getResourceAsStream(path);
-                    if (inputStream != null) {
-                        props.load(inputStream);
-                        loaded = true;
-                        break;
-                    }
-                } catch (IOException e2) {
-                    lastException = e2;
-                }
-            }
-        }
-        
-        if (!loaded) {
-            System.err.println("DEBUG: Failed to load config file. Tried paths:");
-            for (String path : possiblePaths) {
-                System.err.println("  - " + path + " (exists: " + new java.io.File(path).exists() + ")");
-            }
-            System.err.println("DEBUG: Current working directory: " + System.getProperty("user.dir"));
-            throw new RuntimeException("Cannot load config file. Tried paths: " + String.join(", ", possiblePaths), lastException);
+        String path = "src/test/resources/configs/web/" + env + ".properties";
+
+        try (FileInputStream fis = new FileInputStream(path)) {
+            props.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot load config file: " + path, e);
         }
     }
 
